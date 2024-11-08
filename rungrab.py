b6ceaa0c943efa16bacbbaed394f0d1e76909992
A-M <agungmaulana18052000@gmail.com>
2024-11-08 18:50:58 +0700

    Update rungrab.py

--- rungrab.py
+++ rungrab.py
@@ -1,15 +1,16 @@
 import os
 
-print("Welcome to Ftp Tools Anjay")
+print("Welcome to Ftp Tools b3st0fyou")
 print("Pilih Lah:")
 print("1. Ftp Grab")
 print("2. Ftp Split")
 print("3. Ftp Cek")
+print("4. Dorkers")
 
 def screen_clear():
     os.system('cls')
 # Input pilihan pengguna
-pilihan = input("Masukan Pilihan Angka 1, 2, atau 3: ")
+pilihan = input("Masukan Pilihan Angka 1, 2, 3 atau 4: ")
 
 # Cek input pengguna dan jalankan file yang sesuai
 if pilihan == "1":
@@ -18,5 +19,7 @@ elif pilihan == "2":
     os.system("python script/ftpsplit.py")
 elif pilihan == "3":
     os.system("python script/ftpcheck.py")
+elif pilihan == "4":
+    os.system("python script/searcherv3.py")
 else:
-    print("Pilihan tidak valid. Silakan masukkan angka 1, 2, atau 3.")
+    print("Pilihan tidak valid. Silakan masukkan angka 1, 2, 3 atau 4.")
